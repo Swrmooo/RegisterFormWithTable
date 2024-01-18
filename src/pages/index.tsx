02987051ddf1a73d34924aedb7765629aecd62f5
Swrmooo <Siwarut.Moo@gmail.com>
2024-01-18 18:03:06 +0700

    updated UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,94 @@
+import React, { useState, useEffect } from 'react';
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [userInfo, setUserInfo] = useState([]);
   const router = useRouter();
   const handleAddClick = () => {
     router.push('/registerform');
   };
 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
+    setUserInfo(storedData);
+  }, []);
+
   return (
-    <main className='bg-white'>
-      <div className='flex justify-center'>
-      <div>
-        <button type="button"
-        onClick={handleAddClick}
-        className='bg-red-500 p-8 m-4 rounded-xl'  
-        >Add
-        </button>
+    <main className='flex'>
+      <div className='bg-blue-600 rounded-r-xl p-4 w-1/6 h-screen'>
+        <p className='text-white text-5xl text-center'>
+          sidebar
+        </p>
       </div>
-        <table className='w-3/4 m-4'>
-          <tbody>
-          <tr>
-            <th>firstname</th>
-            <th>lastname</th>
-            <th>e-mail</th>
-            <th>tel</th>
-          </tr>
-          <tr>
-            <td>firstname</td>
-            <td>firstname</td>
-            <td>firstname</td>
-            <td>firstname</td>
-          </tr>
-          <tr>
-            <td>firstname</td>
-            <td>firstname</td>
-            <td>firstname</td>
-            <td>firstname</td>
-          </tr>
-          </tbody>
-        </table>
+
+      <div className='w-5/6 h-screen'>
+        <div className='bg-blue-50 h-10'>
+            .
+        </div>
+        
+        <div className='flex justify-between mx-10'>
+          {/* title*/}
+          <p className=' text-5xl mb-4'>
+            Hello !
+          </p>
+
+          <button type="button"
+            onClick={handleAddClick}
+            className='bg-red-500 px-10 rounded-xl'  
+            >Add
+          </button>
+        </div>
+      
+        <div className='bg-yellow-100'>
+          {/* menu bar */}
+          <div className='flex'>
+              <button type="button"
+              onClick={handleAddClick}
+              className='bg-red-500 p-4 rounded-xl'  
+              >Add
+              </button>
+              <button type="button"
+              onClick={handleAddClick}
+              className='bg-red-500 p-4 rounded-xl'  
+              >Add
+              </button>
+              <button type="button"
+              onClick={handleAddClick}
+              className='bg-red-500 p-4 rounded-xl'  
+              >Add
+              </button>
+              
+          </div> 
+
+          {/* table */}
+          <div className='flex'>
+            <table className='w-3/4 m-4'>
+              <thead>
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>E-Mail</th>
+                  <th>Telephone</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userInfo.map((item:any,index:number) => (
+                  <tr key={index}>
+                    <td>{item.fname}</td>
+                    <td>{item.lname}</td>
+                    <td>{item.tel}</td>
+                    <td>{item.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
+
     </main>
   )
 }
--- src/pages/registerform.tsx
+++ src/pages/registerform.tsx
@@ -1,62 +1,95 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function RegisterForm() {
     const router = useRouter();
-    const handleClick = (way:any) => {
-        switch(way) {
-            case "cancel": {
-                router.push('/');
-            }
-            case "submit": {
-                router.push('/');
-            }
-            default: { 
-                break; 
-             }
-        }
+    const [formRegister, setFormRegister] = useState({
+        fname: '',
+        lname: '',
+        tel: '',
+        email: '',
+      });
+
+    //   var regForm = {
+    //     fname: prompt,
+    //     lname: prompt,
+    //     tel: prompt,
+    //     email: prompt,
+    //   }
+
+      const handleInput = (e:any) => {
+        const { name, value } = e.target;
+        setFormRegister((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+
+    //   const saveToLocalStorage = () => {
+    //     // const Info = JSON.stringify(formRegister);
+    //     // localStorage.setItem('Info', Info);
+    //     const Info = JSON.stringify(formRegister);
+    //     localStorage.setItem('Info', Info);
+    //   };
+    
+      const handleSubmit = (e:any) => {
+        e.preventDefault();
+
+        const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
+        storedData.push(formRegister);
+        localStorage.setItem('Info', JSON.stringify(storedData));
         
-    };
+        // saveToLocalStorage();
+
+        router.push('/');
+        // console.log("success", formRegister)
+      };
 
     return (
         <main className=''>
-            <div className='border-2'>
-                <div className='flex justify-center text-5xl mb-4'>
-                    Register
-                </div>
-                <div className='flex flex-col  w-2/6 h-5/6'>       
-                    <input type="text" value="" 
-                    className='border-2'
-                    />
-                    <input type="text" value="" 
-                    className='border-2'
-                    />
-                    <input type="tel" value="" 
-                    className='border-2'
-                    />
-                    <input type="email" value="" 
-                    className='border-2'
-                    />
+            <div className='flex justify-center text-5xl mb-4 border-2'>
+                Register
+            </div>
+            <form className='flex flex-wrap w-1/6 mx-auto'>  
+                <input type="text" id="fname" name="fname" 
+                className='border-2 w-full'
+                onChange={handleInput}
+                value={formRegister.fname}
+                />
+                <input type="text" id="lname" name="lname"
+                className='border-2 w-full'
+                onChange={handleInput}
+                value={formRegister.lname}
+                />
+                <input type="tel" id="tel" name="tel" 
+                className='border-2 w-full'
+                onChange={handleInput}
+                value={formRegister.tel}
+                />
+                <input type="email" id="email" name="email"
+                className='border-2 w-full'
+                onChange={handleInput}
+                value={formRegister.email}
+                />
+            </form>
+            
+            <div className='flex justify-center border-2'>
+                <button type="button"
+                onClick={handleSubmit}
+                className='bg-green-400 p-8 m-4 rounded-xl text-xl'  
+                >submit
+                </button>
 
-                </div>
-                <div className='flex justify-center'>
-                    <button type="button"
-                    onClick={handleClick.bind(null, "submit")}
-                    className='bg-green-400 p-8 m-4 rounded-xl text-xl'  
-                    >submit
-                    </button>
-
-                    <button type="button"
-                    onClick={handleClick.bind(null, "submit")}
-                    className='bg-red-500 p-8 m-4 rounded-xl text-xl'  
-                    >cancel
-                    </button>
-                    
-                </div>
-            </div> 
+                <button type="button"
+                onClick={() => router.push('/')}
+                className='bg-red-500 p-8 m-4 rounded-xl text-xl'  
+                >cancel
+                </button>        
+            </div>
         </main>
   )
 }
