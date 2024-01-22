2c576258b4c601b97c2815411073ec89f1e59fbe
Swrmooo <Siwarut.Moo@gmail.com>
2024-01-22 17:55:07 +0700

    updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,25 @@ import { faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [userInfo, setUserInfo] = useState([]);
   const router = useRouter();
-  const handleAddClick = () => {
-    router.push('/registerform');
-  };
+  const [userInfo, setUserInfo] = useState([]);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [indexToEdit, setIndexToEdit] = useState([]);
+  const [isEdited, setIsEdited] = useState(false);
+  const [editForm, setEditForm] = useState({
+    fname: '',
+    lname: '',
+    tel: '',
+    email: '',
+    date: '',
+  });
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
     setUserInfo(storedData);
-  }, []);
+  }, [userInfo]);
 
-  // const [onHover, setOnHover] = useState(false);
-  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  
   const handleMouseOver = (index: any) => {
     setHoveredIndex(index);
   };
@@ -28,7 +34,33 @@ export default function Home() {
     setHoveredIndex(-1)
   };
 
+  const handleDelete = (indexed: any) => {
+    const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
+    // localStorage.removeItem('Info')
+    const DeletedData = storedData.filter((item:any, index:any) => index !== indexed);
+    localStorage.setItem('Info', JSON.stringify(DeletedData));
+  };
+
+  const handleEdit = (index: any) => {
+    setIndexToEdit(index);
+    const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
+    const itemToEdit = storedData[index];
+
+    setEditForm(itemToEdit);
+    setIsEdited(true);
+  };
+
+  const handleSaveEdit = () => {
+    if (editForm && typeof indexToEdit === 'number') {
+      const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
+      storedData[indexToEdit] = editForm;
   
+      localStorage.setItem('Info', JSON.stringify(storedData));
+  
+      setUserInfo(storedData);
+      setIsEdited(false);
+    }
+  };
 
   return (
     <main className='flex bg-blue-50'>
@@ -52,12 +84,40 @@ export default function Home() {
 
             <div className='my-5 '>
               <button type="button"
-                onClick={handleAddClick}
+                onClick={() => router.push('/registerform')}
                 className='bg-blue-600 text-white py-2 px-5 rounded-xl '  
                 >CREATE NEW
               </button>
             </div>
           </div>
+
+          {isEdited === true &&(
+            <div className='flex justify-between mb-5'>
+              <input type="text" id="fname" name="fname" 
+              value={editForm.fname}
+              onChange={(e) => setEditForm({ ...editForm, fname: e.target.value})}
+              />
+              <input type="text" id="lname" name="lname"
+              value={editForm.lname}
+              onChange={(e) => setEditForm({ ...editForm, lname: e.target.value})}
+              />
+              <input type="tel" id="tel" name="tel"
+              value={editForm.tel}
+              onChange={(e) => setEditForm({ ...editForm, tel: e.target.value})}
+              />
+              <input type="email" id="email" name="email"
+              value={editForm.email}
+              onChange={(e) => setEditForm({ ...editForm, email: e.target.value})}
+              />
+
+              <button type="button"
+               onClick={handleSaveEdit}
+               className='bg-yellow-400 px-2 text-white text-sm rounded'  
+              >
+                <FontAwesomeIcon icon={faPen} style={{fontSize:'10px'}}/>
+              </button>
+            </div>
+          )}
         
           <div className=''>
             {/* table */}
@@ -73,25 +133,27 @@ export default function Home() {
                 </thead>
                 <tbody className='text-lg'>
                   {userInfo.map((item:any,index:number) => (
-                    <tr key={index} className={`hover:bg-slate-200 ease-out duration-300 ${
+                    <tr key={index} className={`hover:bg-slate-200 ease-out duration-300 relative ${
                       index === hoveredIndex ? 'bg-gray-300' : ''
                     }`}
                     onMouseOver={() => handleMouseOver(index)}
                     onMouseOut={handleMouseOut}
                     >
-                      <td>{item.fname}</td>
-                      <td>{item.lname}</td>
-                      <td>{item.tel}</td>
+                      <td>{item?.fname}</td>
+                      <td>{item?.lname}</td>
+                      <td>{item?.tel}</td>
                       <td className='flex justify-between relative'>
-                        <div className='' >{item.email}</div>
+                        <div className='' >{item?.email}</div>
                         {index === hoveredIndex && ( 
                           <div className='flex justify-around w-1/6 absolute right-10 bottom-4'>
                             <button type="button"
+                              onClick={() => handleDelete(index)}
                               className='bg-slate-400 px-2 text-white text-sm rounded '  
                               ><FontAwesomeIcon icon={faTrashCan} style={{fontSize:'10px'}}/>
                             
                             </button>
                             <button type="button"
+                            onClick={() => handleEdit(index)}
                               className='bg-blue-400 px-2 text-white text-sm rounded '  
                               ><FontAwesomeIcon icon={faPen} style={{fontSize:'10px'}}/>
                             </button>
@@ -106,7 +168,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
     </main>
   )
 }
--- src/pages/registerform.tsx
+++ src/pages/registerform.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router';
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faMobileScreen, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,84 +14,141 @@ export default function RegisterForm() {
         lname: '',
         tel: '',
         email: '',
+        date: '',
       });
 
-    //   var regForm = {
-    //     fname: prompt,
-    //     lname: prompt,
-    //     tel: prompt,
-    //     email: prompt,
-    //   }
-
-      const handleInput = (e:any) => {
+      const handleInput = (e:any, inputType:any) => {
+        const { name, value } = e.target;
+        // /^[A-Za-zก-๏]+$/u ไทยและอังกิด /^[A-Za-z]+$/ อังกิดอย่างเดียว
+        // /^\d+$/ ตัวเลข
+        // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/. เมล
+        switch (inputType) {
+          case 'text' : if (/^[A-Za-zก-๏]+$/u.test(value) || value === '') {
+            setFormRegister((prevData) => ({
+              ...prevData,
+              [name]: value,
+            }));
+          }
+          case 'tel' : if (/^\d+$/.test(value) || value === '') {
+            setFormRegister((prevData) => ({
+              ...prevData,
+              [name]: value,
+            }));
+          }
+          case 'email' : setFormRegister((prevData) => ({...prevData, [name]: value,}));
+          default:
+        }
+       
+      };
+      // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.
+      const handleEmailInput = (e:any) => {
         const { name, value } = e.target;
         setFormRegister((prevData) => ({
           ...prevData,
           [name]: value,
         }));
       };
-
-    //   const saveToLocalStorage = () => {
-    //     // const Info = JSON.stringify(formRegister);
-    //     // localStorage.setItem('Info', Info);
-    //     const Info = JSON.stringify(formRegister);
-    //     localStorage.setItem('Info', Info);
-    //   };
     
       const handleSubmit = (e:any) => {
         e.preventDefault();
-
         const storedData = JSON.parse(localStorage.getItem('Info') || '[]');
         storedData.push(formRegister);
         localStorage.setItem('Info', JSON.stringify(storedData));
-        
-        // saveToLocalStorage();
 
         router.push('/');
-        // console.log("success", formRegister)
+        console.log("success", formRegister)
       };
 
     return (
-        <main className=''>
-            <div className='flex justify-center text-5xl mb-4 border-2'>
-                Register
-            </div>
-            <form className='flex flex-wrap w-1/6 mx-auto'>  
-                <input type="text" id="fname" name="fname" 
-                className='border-2 w-full'
-                onChange={handleInput}
-                value={formRegister.fname}
-                />
-                <input type="text" id="lname" name="lname"
-                className='border-2 w-full'
-                onChange={handleInput}
-                value={formRegister.lname}
-                />
-                <input type="tel" id="tel" name="tel" 
-                className='border-2 w-full'
-                onChange={handleInput}
-                value={formRegister.tel}
-                />
-                <input type="email" id="email" name="email"
-                className='border-2 w-full'
-                onChange={handleInput}
-                value={formRegister.email}
-                />
-            </form>
-            
-            <div className='flex justify-center border-2'>
-                <button type="button"
-                onClick={handleSubmit}
-                className='bg-green-400 p-8 m-4 rounded-xl text-xl'  
-                >submit
-                </button>
+        <main className='bg-blue-50 h-screen'>
+          <div className='flex justify-center text-5xl p-10'>
+            Register Form
+          </div>
+          <form className='bg-white shadow-xl shadow-blue-300/100 w-2/6 h-3/4 rounded-xl mx-auto'> 
+            <div className='flex flex-wrap justify-evenly h-full w-full mx-auto px-10'>
+              <div className='font-semibold text-lg pt-10'>
+                Hello! Welcome
+              </div>
 
-                <button type="button"
+              <div className='flex relative items-center justify-center shadow-xl shadow-blue-300/30 w-full h-10 rounded-3xl'>
+                <FontAwesomeIcon icon={faUser} style={{position:'absolute', left:'20px', color:'grey', fontSize:'15px'}}/>
+                <input type="text" id="fname" name="fname" 
+                  className='w-full h-full rounded-3xl pl-10'
+                  // onChange={handleTextInput}
+                  onChange={(e) => handleInput(e, 'text')}
+                  placeholder='Firstname'
+                  maxLength={20}
+                  value={formRegister.fname}
+                  title="Please enter only letters"
+                  required
+              />
+              </div>
+              
+              <div className='flex relative items-center justify-center shadow-xl shadow-blue-300/30 w-full h-10 rounded-3xl'>
+                <FontAwesomeIcon icon={faUser} style={{position:'absolute', left:'20px', color:'grey', fontSize:'15px'}}/>
+                <input type="text" id="lname" name="lname" 
+                  className='w-full h-full rounded-3xl pl-10'
+                  onChange={(e) => handleInput(e, 'text')}
+                  placeholder='Lastname'
+                  maxLength={20}
+                  title="Please enter only letters"
+                  value={formRegister.lname}
+                  required
+              />
+              </div>
+
+              <div className='flex relative items-center justify-center shadow-xl shadow-blue-300/30 w-full h-10 rounded-3xl'>
+                <FontAwesomeIcon icon={faMobileScreen} style={{position:'absolute', left:'20px', color:'grey', fontSize:'15px'}}/>
+                <input type="text" id="tel" name="tel" 
+                  className='w-full h-full rounded-3xl pl-10'
+                  onChange={(e) => handleInput(e, 'tel')}
+                  placeholder='Telephone Number'
+                  maxLength={10}
+                  title="Please enter only numbers"
+                  value={formRegister.tel}
+                  required
+              />
+              </div>
+
+              <div className='flex relative items-center justify-center shadow-xl shadow-blue-300/30 w-full h-10 rounded-3xl'>
+                <FontAwesomeIcon icon={faEnvelope} style={{position:'absolute', left:'20px', color:'grey', fontSize:'15px'}}/>
+                <input type="text" id="email" name="email" 
+                  className='w-full h-full rounded-3xl pl-10'
+                  onChange={(e) => handleInput(e, 'email')}
+                  maxLength={30}
+                  placeholder='E-Mail'
+                  value={formRegister.email}
+                  required
+              />
+              </div>
+
+              {/* button */}
+              <div className='w-full flex flex-col items-center'>
+                <div className='flex justify-center w-3/4 text-white'>
+                  <button type="button"
+                  onClick={handleSubmit}
+                  className='bg-gradient-to-r shadow-xl shadow-blue-300/30 from-cyan-200 to-blue-500 h-10 
+                  font-semibold w-3/6 rounded-xl text-lg hover:bg-gradient-to-l duration-800 ease-linear'  
+                  >submit
+                  </button>
+                  
+                </div>
+                <p className='text-blue-500 text-xs font-semibold cursor-pointer py-5 hover:text-gray-800'
                 onClick={() => router.push('/')}
-                className='bg-red-500 p-8 m-4 rounded-xl text-xl'  
-                >cancel
-                </button>        
-            </div>
+                >
+                    Already Have Account ?
+                </p>
+
+                {/* <div>
+                  <button type="button"
+                    onClick={() => router.push('/')}
+                    className='bg-red-500  rounded-xl text-lg'  
+                    >cancel
+                  </button>  
+                </div>     */}
+              </div>
+            </div> 
+          </form>
         </main>
   )
 }
